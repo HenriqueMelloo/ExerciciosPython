11c65444b0b290ededb79cd73fb620a1ac7d4ef5
h3nriquemelo <h3nriquemelo@gmail.com>
2023-11-04 20:19:43 -0400

    Verificar versão do Python

---
+++ Exercicio01.py
@@ -0,0 +1,9 @@
+# Faça um programa que informe a versão do Python que você esta utilizando.
+
+import sys
+
+print("Versão do Python")
+print(sys.version)
+
+print("Informações da versão instalada")
+print (sys.version_info)
